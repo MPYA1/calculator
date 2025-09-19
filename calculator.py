4cbe6248d42b79378aa2007336db1ca7e3aa398e
Yasin Afras <mrversion35@gmail.com>
2025-09-19 11:23:55 +0330

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+from sqrtmath import num_root
+def cal_num(a,b:float):
+    if not isinstance(a, float) or not isinstance(b, float):
+        raise TypeError("num must be  int")
+    print("1-> ADD,2-> <-> , 3->MUL,4->div,5->pow,sqrt->6")
+    choice=int(input("please enter your number:"))
+    if choice==1:
+        return a+b
+    elif choice==2:
+        return a-b
+    elif choice==3:
+        return a*b
+    elif choice==4:
+        return a/b
+    elif choice==5:
+        return a**b
+    elif choice==6:
+        return num_root(a,b)
+
+print(cal_num(8,3))
